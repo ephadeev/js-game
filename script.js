a3f1948280ca7a53d82631ca00445fd1868747c6
ephadeev <itrex1991@hotmail.com>
2020-09-09 22:20:52 +0300

    add some code

--- script.js
+++ script.js
@@ -21,15 +21,72 @@ const setting = {
     speed: 3
 }
 
+const getQuantityOfElements = heightOfElement => {
+    return document.documentElement.clientHeight / heightOfElement + 1;
+}
+
 const startGame = () => {
+    for (let i = 0; i < 20; i++) {
+        const stars = document.createElement('div');
+        stars.classList.add('star');
+        stars.style.top = 0;
+        stars.y = 0;
+    }
+
+    for (let i = 0; i < 20; i++) {
+        const enemy = document.createElement('div');
+        enemy.classList.add('enemy');
+        enemy.y = 100 * i + 1;
+        enemy.style.top = `${enemy.y}px`;
+        gameArea.appendChild(enemy);
+    }
+
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
+
+const moveEnemies = () => {
+    let enemies = document.getElementsByClassName('enemy');
+
+    for (let i = 0; i < enemies.length; i++) {
+        enemies[i].y = Math.random() * 400;
+        enemies[i].style.top = `${enemies[i].y}px`;
+        enemies[i].style.left = `${Math.floor(Math.random() * gameArea.offsetWidth - enemies[i].offsetWidth)}px`;
+    }
+};
+const moveStars = () => {
+    return 5;
+};
 
 const playGame = () => {
     console.log('play game');
     if (setting.start) {
+        moveEnemies();
+        moveStars();
+
+
+
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+
+        car.style.left = `${setting.x}px`;
+        car.style.top = `${setting.y}px`;
         requestAnimationFrame(playGame);
     }
 }
