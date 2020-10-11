83bc49b2b1d0d141281ab02ed5985926c47d657d
ephadeev <itrex1991@hotmail.com>
2020-10-11 19:01:30 +0300

    added ability to use W A S D keys

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ const score = document.getElementsByClassName('score')[0];
 const game = document.getElementsByClassName('game')[0];
 const start = document.getElementsByClassName('start')[0];
 const gameArea = document.getElementsByClassName('gameArea')[0];
-const car = document.createElement('div');
+const player = document.createElement('div');
 
-car.classList.add('car');
+player.classList.add('player');
 
 const keys = {
     ArrowUp: false,
     ArrowDown: false,
     ArrowRight: false,
-    ArrowLeft: false
+    ArrowLeft: false,
+    w: false,
+    s: false,
+    d: false,
+    a: false
 }
 
 const setting = {
@@ -26,6 +30,9 @@ const getQuantityOfElements = heightOfElement => {
 }
 
 const startGame = () => {
+    start.style.display = 'none';
+    
+    // create stars
     for (let i = 0; i < 20; i++) {
         const stars = document.createElement('div');
         stars.classList.add('star');
@@ -33,18 +40,22 @@ const startGame = () => {
         stars.y = 0;
     }
 
-    for (let i = 0; i < 20; i++) {
+    // create enemies
+    for (let i = 0; i < 3; i++) {
         const enemy = document.createElement('div');
         enemy.classList.add('enemy');
-        enemy.y = 100 * i + 1;
+        enemy.x = Math.floor(Math.random() * 400);
+        enemy.y = Math.floor(Math.random() * 400);
+        enemy.style.left = `${enemy.x}px`;
         enemy.style.top = `${enemy.y}px`;
         gameArea.appendChild(enemy);
     }
 
     setting.start = true;
-    gameArea.appendChild(car);
-    setting.x = car.offsetLeft;
-    setting.y = car.offsetTop;
+    gameArea.appendChild(player);
+    player.x = player.offsetLeft;
+    player.y = player.offsetTop;
+    console.log(player.getBoundingClientRect());
     requestAnimationFrame(playGame);
 }
 
@@ -64,47 +75,41 @@ const moveStars = () => {
 const playGame = () => {
     console.log('play game');
     if (setting.start) {
-        moveEnemies();
-        moveStars();
-
-
-
-        if (keys.ArrowLeft && setting.x > 0) {
-            setting.x -= setting.speed;
+        
+        if ((keys.ArrowLeft || keys.a) && player.x > 0) {
+            player.x -= setting.speed;
         }
 
-        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
-            setting.x += setting.speed;
+        if ((keys.ArrowRight || keys.d) && player.x < (gameArea.offsetWidth - player.offsetWidth)) {
+            player.x += setting.speed;
         }
 
-        if (keys.ArrowUp && setting.y > 0) {
-            setting.y -= setting.speed;
+        if ((keys.ArrowUp || keys.w) && player.y > 0) {
+            player.y -= setting.speed;
         }
 
-        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
-            setting.y += setting.speed;
+        if ((keys.ArrowDown || keys.s) && player.y < (gameArea.offsetHeight - player.offsetHeight)) {
+            player.y += setting.speed;
         }
 
-        car.style.left = `${setting.x}px`;
-        car.style.top = `${setting.y}px`;
+        player.style.left = `${player.x}px`;
+        player.style.top = `${player.y}px`;
         requestAnimationFrame(playGame);
     }
 }
 
-const startRun = e => {
-    e.preventDefault();
-    keys[e.key] = true;
-    console.log(e.key);
+const startRun = event => {
+    event.preventDefault();
+    keys[event.key] = true;
 }
 
-const stopRun = e => {
-    e.preventDefault();
-    keys[e.key] = false;
-    console.log(e.key);
+const stopRun = event => {
+    event.preventDefault();
+    keys[event.key] = false;
 }
 
 document.addEventListener('keydown', startRun);
 document.addEventListener('keyup', stopRun);
 
 
-start.addEventListener('click', startGame)
+start.addEventListener('click', startGame);
